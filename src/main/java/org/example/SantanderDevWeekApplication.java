47fc8532d45c6c1dbccab7e390c6ff79aa46bae9
Bruno <brunofraga19@gmail.com>
2025-07-23 20:12:59 -0300

    Corrigido o cors do swagger

--- src/main/java/org/example/SantanderDevWeekApplication.java
+++ src/main/java/org/example/SantanderDevWeekApplication.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//coreção do cors no swagger
 @OpenAPIDefinition(servers = { @Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication
 public class SantanderDevWeekApplication {
